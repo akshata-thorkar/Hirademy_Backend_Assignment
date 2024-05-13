86887597f25864f18c85840bd62aeb9d7c966a25
akshata-thorkar <144578044+akshata-thorkar@users.noreply.github.com>
2024-05-13 21:59:53 +0530

    AssistantsController.java

--- assignment/src/main/java/com/hirademy/assignment/controller/AssistantsController.java
+++ assignment/src/main/java/com/hirademy/assignment/controller/AssistantsController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/assistants")
+@RequestMapping("/assistant")
 public class AssistantsController {
 
     @Autowired
